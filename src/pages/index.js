97714ccca87c41d6452e89c22894f68acbf170ed
Parulllllllll <96309177+Parulllllllll@users.noreply.github.com>
2021-12-17 22:47:17 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi peoplemyself parull </h1>
+    <p>Welcome to your new Gatsby site of parul .</p>
+    <p>Now go build something great okkk.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -20,10 +20,10 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
+      <Link to="/page-2/">Go to page 1 2</Link> <br />
+      <Link to="/using-typescript/">Go to 2 "Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to  3"Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to 4 "Using DSG"</Link>
     </p>
   </Layout>
 )
